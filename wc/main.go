e7211192634566a452fbd27b5ceaa08ae55ea4f8
thenoobgrammer <thenoobgrammer@gmail.com>
2025-03-21 18:14:18 -0400

    Adding todo + tests

--- wc/main.go
+++ wc/main.go
@@ -10,14 +10,18 @@ import (
 
 func main() {
 	lines := flag.Bool("l", false, "Count line")
+	bytes := flag.Bool("b", false, "Count bytes")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
 	if !countLines {
 		scanner.Split(bufio.ScanWords)
+	}
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 	wc := 0
 
